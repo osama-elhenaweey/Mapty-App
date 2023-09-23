0192be94c455f6895420542b879622eaa8ce18bc
Osama El Henaweey <87882318+osama-elhenaweey@users.noreply.github.com>
2023-09-23 22:27:34 +0300

    ✅Commiting✅

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const coords = [latitude, longitude];
+            const map = L.map("map").setView(coords, 13);
+
+            L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map);
+
+            map.on("click", function (mapEvent) {
+                const { lat, lng } = mapEvent.latlng;
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup({
+                            maxWidth: 250,
+                            minWidth: 100,
+                            autoClose: false,
+                            closeOnClick: false,
+                            className: "running-popup",
+                        })
+                    )
+                    .setPopupContent("workout")
+                    .openPopup();
+            });
+        },
+        function () {
+            alert("Could not get your location");
+        }
+    );
+}
